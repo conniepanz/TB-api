60a1ddc670d6071a89ff4689487de6aa6d85e1cc
Connie <connie.panzariello@gmail.com>
2021-04-14 22:35:00 -0400

    2nd commit finishing backend

---
+++ curl-scripts/topics/update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+API="http://localhost:4741"
+URL_PATH="/topics"
+
+curl ""${API}${URL_PATH}/${ID}"" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "topic": {
+      "username": "'"${USERNAME}"'",
+      "topic": "'"${TOPIC}"'",
+      "comment": "'"${COMMENT}"'"
+    }
+  }'
+
+echo
